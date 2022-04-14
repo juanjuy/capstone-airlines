6b5262e1adde5facbfbf4b64cc8a827f82317db7
rjg <gorman.rob.j@gmail.com>
2022-04-14 19:48:30 -0400

    stage5 preliminary

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react';
+import { useState } from 'react';
 import './App.css';
 import data from './data';
+import { getAirlineById, getAirportByCode } from './data.js';
 import { Table } from './components/Table';
 
 const App = () => {
+  const [ currentPage, updateCurrentPage ] = useState(2);
+  const [ currentRoutes, updateCurrentRoutes ] = useState(data.routes)
+  var rowsPerPage = 25
+  const [ pages, updatePages ] = useState(buildPages(rowsPerPage)) // updatePages(buildPages())
+
+  const filterRoutes = () => {
+    // stand-in value, WIP
+  }
+
+  const displayedRoutes = () => {
+    const pagesIndex = currentPage - 1
+    return pages[pagesIndex]
+  }
+
+  function buildPages(perPage) {
+    const data = currentRoutes;
+    const numberOfPages = Math.ceil(data.length / perPage)
+    const lastEntryIndex = data.length - 1
+    let pagesArr = [];
+    for (let n = 1; n <= numberOfPages; n++) {
+      let page = [];
+      for (let rowNum = 1; rowNum <= perPage; rowNum++) {
+        let dataEntryIndex = rowNum + (perPage * n) - 1
+        if (dataEntryIndex > lastEntryIndex) break
+        page = page.concat(data[dataEntryIndex])
+      }
+      pagesArr.push(page)
+    }
+    console.log(pagesArr);
+    return pagesArr
+  }
+
+  const validateButton = (buttonType) => { 
+    if (buttonType === 'previous' && currentPage === 1) return true
+    if (buttonType === 'next' && currentPage === pages.length) return true
+    return false
+  }
 
   const formatValues = (property, value) => {
     // returns either airline name or airport name based on column
     if (property === 'airline') {
-      return data.getAirlineById(value).name
+      return getAirlineById(value).name
     } else {
-      return data.getAirportByCode(value).name
+      return getAirportByCode(value).name
     }
   }
 
@@ -22,14 +61,30 @@ const App = () => {
   
   return (
     <div className="app">
-    <header className="header">
-      <h1 className="title">Airline Routes</h1>
-    </header>
-    <section>
-    <Table className="routes-table" columns={ columns } rows={ data.routes } format={ formatValues } />
-    </section>
-  </div>
+      <header className="header">
+        <h1 className="title">Airline Routes</h1>
+      </header>
+      <section>
+        <Table className="routes-table" columns={ columns } rows={ displayedRoutes() } format={ formatValues } />
+        <NavigationMessage currentPage={currentPage} totalEntries={currentRoutes.length} perPage={rowsPerPage}/>
+        <div>
+          <button disabled={ validateButton('previous') } onClick={() => updateCurrentPage(currentPage - 1)}>Previous Page</button>
+          <button disabled={ validateButton('next') } onClick={() => updateCurrentPage(currentPage + 1)}>Next Page</button>
+        </div>
+      </section>
+    </div>
   )
 }
+
+function NavigationMessage({currentPage, totalEntries, perPage}) {
+  let firstEntry = ( currentPage - 1 ) * perPage + 1
+  let lastEntry
+  if (currentPage * perPage > totalEntries) {
+    lastEntry = totalEntries
+  } else lastEntry = firstEntry + perPage - 1
+
+  return <div>Showing {firstEntry}-{lastEntry} of {totalEntries} routes.</div>
+}
+
 
 export default App;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getAirlineById, getAirportByCode } from '../data.js';
+// import { getAirlineById, getAirportByCode } from '../data.js';
 /*
 columns is array of column headers
 rows is array of all data.routes
@@ -7,27 +7,28 @@ formatValue is a function that returns a string (airport or airline)
 
 */
 
-const Table = ({ columns, rows, formatValues }) => {
-  console.log('columns', columns);
-  console.log('rows', rows);
-  console.log('formatValues', formatValues);
+export const Table = ({ className, columns, rows, format }) => {
+  // console.log('columns', columns);
+  // console.log('rows', rows);
+  // console.log('formatValues', format);
 
   return (
-    <table>
+    <table className={className}>
       <thead>
-      <tr>
-        { columns.map(column =>
-            <td>column.name</td>
-        )}
+        <tr>
+          { columns.map((column, index) =>
+              <th key={index + 1}>{column.name}</th>
+          )}
         </tr>
       </thead>
       <tbody>
-      { rows.map(row => {
+      { rows.map((row, index) => {
         return (
-      <tr>
-        <td>{ formatValues(columns[0].property, row.columns[0].property) }</td>
-        <td>{ formatValues(columns[1].property, row.columns[1].property) }</td>
-        <td>{ formatValues(columns[2].property, row.columns[2].property) }</td>
+      <tr key={index + 1}>
+        {console.log("current row, ", row)}
+        <td>{ format(columns[0].property, row[columns[0].property]) }</td>
+        <td>{ format(columns[1].property, row[columns[1].property]) }</td>
+        <td>{ format(columns[2].property, row[columns[2].property]) }</td>
       </tr>
     )
   })
